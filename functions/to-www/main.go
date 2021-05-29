daf00cc8e4ff367fc41a28c20ea88669d65b8015
Stephane Bruckert <stephane.bruckert@gmail.com>
2021-05-29 14:52:57 +0100

    Set cors origin

--- functions/to-www/main.go
+++ functions/to-www/main.go
@@ -168,7 +168,7 @@ func main() {
 
 func cors(c *gin.Context) {
 	c.Header("Content-Type", "application/json")
-	c.Header("Access-Control-Allow-Origin", "*")
+	c.Header("Access-Control-Allow-Origin", "https://mirror.fm")
 	c.Header("Access-Control-Allow-Credentials", "true")
 	c.Header("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
 	c.Header("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
