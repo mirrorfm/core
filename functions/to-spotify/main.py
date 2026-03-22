e21d39f346fbc5866ea9cb8df940de03f5fbef7a
Stephane Bruckert <stephane.bruckert@gmail.com>
2026-03-22 11:30:14 +0000

    Reapply "Fix to-spotify token expiry and MySQL timeout during long batches"
    This reverts commit 8346ef5c8cb9dbd241181dd646d864b45d523579.

--- functions/to-spotify/main.py
+++ functions/to-spotify/main.py
@@ -77,7 +77,7 @@ class Memoize:
 PLAYLIST_EXPECTED_MAX_LENGTH = 11000
 WEBSITE = "https://mirror.fm"
 
-BATCH_GET_SIZE = 1000
+BATCH_GET_SIZE = 200
 
 # DB
 client = boto3.client("dynamodb", region_name='eu-west-1')
@@ -208,7 +208,8 @@ def get_spotify():
         raise (Exception('null token_info'))
     store_spotify_token(token_info)
 
-    return spotipy.Spotify(auth=token_info['access_token'], retries=3, status_retries=3)
+    # Use auth_manager instead of static token so spotipy auto-refreshes on 401
+    return spotipy.Spotify(auth_manager=sp_oauth, retries=3, status_retries=3)
 
 
 def find_youtube_track_on_spotify(handler, track_name):
